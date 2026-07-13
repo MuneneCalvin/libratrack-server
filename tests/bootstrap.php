4525f8b9b629c3fef9420ae702b0ba67feefaa8f
MuneneCalvin <munenecalvn@gmail.com>
2026-07-13 14:53:11 +0300

    chore: scaffold plain PHP backend runtime

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$_ENV['APP_ENV'] = 'test';
+$_ENV['JWT_SECRET'] = 'test-secret';
+$_ENV['JWT_ACCESS_TTL_MINUTES'] = '15';
+$_ENV['JWT_REFRESH_TTL_DAYS'] = '7';
